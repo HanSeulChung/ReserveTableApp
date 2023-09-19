0135a2b7edea66c90b07dd2d68147e6cf28a0d44
HanSeul <w0w1278@naver.com>
2023-09-19 13:39:22 +0900

    init

---
+++ src/main/java/com/chs/ReserveTableAppApplication.java
@@ -0,0 +1,13 @@
+package com.chs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveTableAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveTableAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chs/ReserveTableAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveTableAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
